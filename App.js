005e6a93d3d63f5b991f81dcbe1c14a1cf8842c7
stuti1312 <stutisinghal1312@gmail.com>
2023-06-12 17:21:48 +0530

    full navigation set up done

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import AppNavigator from './src/AppNavigator'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <AppNavigator/>
   )
 }
 
 export default App
-
-const styles = StyleSheet.create({})
---
+++ src/screens/SideMenu.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SideMenu = () => {
+  return (
+    <View>
+      <Text>SideMenu</Text>
+    </View>
+  )
+}
+
+export default SideMenu
