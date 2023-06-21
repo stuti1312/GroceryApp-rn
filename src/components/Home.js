0c761160cfed2acebaf612cac2a1e0dee22374f4
stuti1312 <stutisinghal1312@gmail.com>
2023-06-22 01:42:51 +0530

    product list implemented

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,70 @@
-import {View, StyleSheet} from 'react-native';
-import React from 'react';
+import {View, StyleSheet, FlatList, Text, Dimensions} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import CustomHeader from '../reusables/CustomHeader';
+// import {useNavigation} from '@react-navigation/native';
+// import Cart from './Cart';
+import Colors from '../constants/Colors';
+import CustomImage from '../reusables/CustomImage';
 
+// const Home = ({navigation}) => {
 const Home = () => {
+  const navigation = useNavigation();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(json => setProducts(json));
+  };
+
+  const renderItem = ({item}) => {
+    return (
+      <View style={styles.listItems}>
+        <CustomImage
+          imageSrc={{uri: item.image}}
+          imageStyle={styles.productImage}
+        />
+        <View style={styles.itemDetails}>
+          <Text style={styles.name}>
+            {item.title.length > 30
+              ? item.title.substring(0, 25) + '...'
+              : item.title}
+          </Text>
+          <Text style={styles.desc}>
+            {item.description.length > 40
+              ? item.description.substring(0, 35) + '...'
+              : item.description}
+          </Text>
+          <Text style={styles.price}>${item.price}</Text>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <CustomHeader
         leftIcon={require('../assests/icons/menu.png')}
-        onClickLeftIcon={() => console.log('left clicked')}
+        onClickLeftIcon={() => {
+          // navigation.openDrawer();
+          // console.log('left clicked');
+        }}
         rightIcon={require('../assests/icons/cart.png')}
-        onClickRightIcon={() => console.log('right clicked')}
+        onClickRightIcon={() => {
+          // navigation.navigate('cart');
+        }}
         title="Grocery App"
+      />
+      <FlatList
+        data={products}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        // numColumns={2}
+        style={styles.list}
       />
     </View>
   );
@@ -20,4 +74,18 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {flex: 1},
+  list: {marginHorizontal: 10},
+  listItems: {
+    width: Dimensions.get('window').width,
+    height: 100,
+    backgroundColor: Colors.WHITE,
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  productImage: {width: 100, height: 100, margin: 10, alignSelf: 'center'},
+  itemDetails:{padding:10,},
+  name: {fontSize: 18, fontWeight: '600'},
+  desc: {},
+  price: {fontSize: 18, fontWeight: '600', color: 'green',marginTop:10},
 });
