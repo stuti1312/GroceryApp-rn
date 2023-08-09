ca2a545a353f71b212cb636df75ccba96e881335
stuti1312 <stutisinghal1312@gmail.com>
2023-08-09 16:19:11 +0530

    push notification permission popup

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-import React from 'react';
 import {Provider} from 'react-redux';
 import AppNavigator from './src/components/AppNavigator';
 import {store} from './src/redux/store';
+import React, {useEffect} from 'react';
+import {PermissionsAndroid} from 'react-native';
 
 const App = () => {
+  //for permission request popup(allow/deny)
+  useEffect(() => {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+    );
+  }, []);
+
   return (
     <Provider store={store}>
       <AppNavigator />
