78c8f3f91f7670a403d29337711b6d7b732ba6dd
stuti1312 <stutisinghal1312@gmail.com>
2023-08-09 16:21:39 +0530

    kill mode push notification

--- index.js
+++ index.js
@@ -12,4 +12,9 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
 });
 
+// Register kill mode handler
+messaging().getInitialNotification(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+
 AppRegistry.registerComponent(appName, () => App);
