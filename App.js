f2b5abb44aa771f6bd68de75130f50650224e5af
stuti1312 <stutisinghal1312@gmail.com>
2023-08-09 16:20:27 +0530

    foreground push notification

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import {Provider} from 'react-redux';
 import AppNavigator from './src/components/AppNavigator';
 import {store} from './src/redux/store';
+import {Alert} from 'react-native';
 import React, {useEffect} from 'react';
 import {PermissionsAndroid} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
   //for permission request popup(allow/deny)
@@ -12,6 +14,24 @@ const App = () => {
     );
   }, []);
 
+  //for getting foreground notification
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    getDeviceToken();
+  }, []);
+
+  // required for generating push notification
+  const getDeviceToken = async () => {
+    const token = await messaging().getToken();
+    console.log('token', token);
+  };
+  
   return (
     <Provider store={store}>
       <AppNavigator />
